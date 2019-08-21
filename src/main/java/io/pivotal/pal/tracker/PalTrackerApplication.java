078b4a99012cc5392e26fe6c74926d9ed147bd15
richa_verma2 <richa_verma2@dell.com>
2019-08-21 19:20:15 +0530

    JDBC Template

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,17 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
-
-    @Bean
-    TimeEntryRepository timeEntryRepository() {
-
-        return new InMemoryTimeEntryRepository();
-    }
 
     public static void main(String[] args) {
 
         SpringApplication.run(PalTrackerApplication.class, args);
+    }
+
+    @Bean
+    TimeEntryRepository timeEntryRepository(DataSource dataSource) {
+
+        return new JdbcTimeEntryRepository(dataSource);
     }
 }
